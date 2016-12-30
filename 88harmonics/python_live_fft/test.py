dc690a07dcce329293ace2c00f9c0de78177c485
emouty <erwan.moutymbo@gmail.com>
2016-12-30 16:01:39 +0100

    add fftBlack delete readStream because callback mode is better add description of methods

--- 88harmonics/python_live_fft/test.py
+++ 88harmonics/python_live_fft/test.py
@@ -1,7 +1,7 @@
 import pyaudio as pa
 import numpy as np
 from matplotlib import pyplot as plt
-from pylab import plot, show, title, xlabel, ylabel, subplot
+from pylab import xscale, figure, plot, show, title, xlabel, ylabel, subplot
 from scipy import fft, arange, signal
 import struct
 import time
@@ -46,13 +46,41 @@ def plotSpectrum(y, Fs):
     n = len(y)  # length of the signal
     k = arange(n)
     T = n / Fs
+    end = int(n / 10)  # default put n/2
     frq = k / T  # two sides frequency range
-    frq = frq[range(int(n / 2))]  # one side frequency range
+    frq = frq[range(end)]  # one side frequency range
     y = y * signal.blackmanharris(len(y))
     Y = fft(y) / n  # fft computing and normalization
-    Y = Y[range(int(n / 2))]
-
+    Y = Y[range(end)]
+    figure(1)
     plot(frq, abs(Y), 'r')  # plotting the spectrum
+    #xscale('log')
+    xlabel('Freq (Hz)')
+    ylabel('|Y(freq)|')
+    # show()
+    return Y,
+
+
+def plotSpectrum2(y, Fs):
+    """
+    Plots a Single-Sided Amplitude Spectrum of y(t)
+    :param y: audio data
+    :param Fs:Rate
+    :return:
+    """
+    n = len(y)  # length of the signal
+    k = arange(n)
+    T = n / Fs
+    end = int(n / 10)  # default n/2
+    frq = k / T  # two sides frequency range
+    frq = frq[range(end)]  # one side frequency range
+    y = y * signal.blackmanharris(len(y))
+    Y = fft(y) / n  # fft computing and normalization
+    Y = Y[range(end)]
+
+    figure(2)
+    plot(frq, abs(Y), 'b')  # plotting the spectrum
+    # xscale('log')
     xlabel('Freq (Hz)')
     ylabel('|Y(freq)|')
     show()
@@ -66,20 +94,25 @@ def findFreq(spec):
     """
     spec = abs(np.array(spec))
     # i : indice of max amp
-    i = np.argmax(spec[0])
-    i = parabolic(np.log(spec[0]), i)[0]
-    return RATE * i / len(spec[0]), i
+    try:
+        i = np.argmax(spec)
+        i = parabolic(np.log(spec), i)[0]
+        return RATE * i / len(spec)
+    except IndexError:
+        i = np.argmax(spec[0])
+        i = parabolic(np.log(spec[0]), i)[0]
+        return RATE * i / len(spec[0])
 
 
 def harmonicMode(data, freq):
     """
     suppress fundamental
-    :param data: frequence
+    :param data: frequence?
     :param freq: center of filter
     :return: filtered data
     """
-    freqMin = freq * 0, 9
-    freqMax = freq * 1, 1
+    freqMin = int(freq * 0.9)
+    freqMax = int(freq * 1.1)
     return bandstop(data, freqMin, freqMax, RATE)
 
 
@@ -95,8 +128,10 @@ def callback(in_data, frame_count, time_info, flag):
     global b, a, fulldata, dry_data, frames
     audio_data = np.fromstring(in_data, dtype=np.float32)
     # do processing here
-
-    # fulldata = np.append(fulldata,audio_data)
+    # audioInFreq = fftBlack(audio_data)
+    # fulldata = abs(np.fft.ifft(harmonicMode(audioInFreq, findFreq(audioInFreq))))
+    # only for time domain
+    fulldata = np.append(fulldata, audio_data)
     return (audio_data, pa.paContinue)
 
 
@@ -110,12 +145,14 @@ stream = soundObject.open(format=FORMAT,
 if __name__ == '__main__':
     while stream.is_active():
         print("actif")
-        time.sleep(2)
+        time.sleep(1)
         stream.stop_stream()
     stream.close()
 
+    #use line below if fulldata is a sum
     frames = np.hstack(fulldata)
-
+    # else
+    #frames = fulldata
     # readData = readstream(stream)
 
     # i = 0
@@ -127,8 +164,27 @@ if __name__ == '__main__':
     #
     # #Y_R = np.fft.fft(readData, nFFT)
     #
+    # filter response
+    freqC = findFreq(frames)
+    b, a = signal.butter(4, (0.9 * freqC, 1.1 * freqC), 'bandstop', analog=True)
+    w, h = signal.freqs(b, a)
+    plt.figure(3)
+    plt.plot(w, 20 * np.log10(abs(h)))
+    plt.xscale('log')
+    plt.title('Butterworth filter frequency response')
+    plt.xlabel('Frequency [radians / second]')
+    plt.ylabel('Amplitude [dB]')
+    plt.margins(0, 0.1)
+    plt.grid(which='both', axis='both')
+    plt.axvline(freqC, color='green')  # cutoff frequency
 
+    # tests
+    print("ploting")
     spectrum = plotSpectrum(frames, RATE)
+    print("freq")
+    print(findFreq(frames))
+    spectrum = plotSpectrum2(harmonicMode(frames, findFreq(frames)),RATE)
+
 
     # stream.close()
     soundObject.terminate()
