24a71abc258c93619e8c6de393244754a7910ad8
emouty <erwan.moutymbo@gmail.com>
2017-01-05 12:44:55 +0100

    9chip program

---
+++ 88harmonics/python_live_fft/tds-9chip.py
@@ -0,0 +1,176 @@
+import pyaudio as pa
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib.mlab import find
+from pylab import xscale, figure, plot, show, title, xlabel, ylabel, subplot
+from scipy import fft, arange, signal, ifft
+import struct
+import time
+from parabolic import parabolic
+from obspy.signal.filter import bandstop, highpass
+from bluetooth import *
+
+#pyaudio config
+soundObject = pa.PyAudio()
+
+nFFT = 1028
+BUF_SIZE = 4 * nFFT
+FORMAT = pa.paFloat32
+CHANNELS = 2
+RATE = 44100
+RECORD_SECONDS = 3
+
+# bluetooth config
+
+HOST = "A4:D1:8C:D2:52:39"      # The remote host
+PORT =  3              # Server port
+
+#global variable
+indiceOfFundamental = 0
+harmonic = False
+freq = 440
+
+
+def fftBlack(y):
+    """
+    do fft with blackmanharris window
+    :param y: audio in
+    :return: freq domain
+    """
+    n = len(y)  # length of the signal
+    y = y * signal.blackmanharris(len(y))
+    Y = fft(y) / n  # fft computing and normalization
+    Y = Y[range(int(n / 2))]
+    return Y,
+
+
+def findFreq(spec, inFreq = True):
+    """
+
+    :param spec: full spectrum
+    :param inFreq:
+    :return: fundamental frequency
+    """
+    global indiceOfFundamental
+
+
+    if not(inFreq):
+        spec = abs(np.array(fftBlack(spec)))
+    else:
+        spec = abs(np.array(spec))
+
+    # i : indice of max
+    print("spec in findFreq")
+    print(spec)
+
+    try:
+        i = np.argmax(spec)
+        print("i in findFreq")
+        print(i)
+        i = parabolic(np.log(spec), i)[0]
+        print("i parabolic")
+        print(i)
+        indiceOfFundamental = int(i)
+        return RATE * i / len(spec)
+    except IndexError:
+        i = np.argmax(spec[0])
+        i = parabolic(np.log(spec[0]), i)[0]
+        indiceOfFundamental = int(i)
+        return RATE * i / len(spec[0])
+
+
+def amplificationHarmonic(data):
+    """
+    call findFreq before this method
+    This method amplify the value of the
+    :param data:
+    :return:
+    """
+    shift = 50
+    numberOfPoints = shift*2
+    gaussian = signal.gaussian(numberOfPoints, std=7)
+    dataFreq = fftBlack(data)
+    dataFreq = dataFreq[0]
+    print("lenght of data")
+    print(len(dataFreq))
+    for i in range(numberOfPoints):
+        try:
+            print("index")
+            print(indiceOfFundamental * 2 - shift + i)
+            print("before")
+
+            print(dataFreq[indiceOfFundamental*2-shift+i])
+            dataFreq[indiceOfFundamental*2-shift+i] += dataFreq[indiceOfFundamental*2-shift+i]*10**(5*gaussian[i])
+            print("after")
+            print(dataFreq[indiceOfFundamental * 2 - shift + i])
+            print("")
+        except IndexError:
+            print("index error")
+    #print(np.real(ifft(dataFreq)))
+    result = np.real(ifft(dataFreq))
+    return result
+
+
+def harmonicMode(data, freq):
+    """
+    suppress fundamental
+    :param data: frequence?
+    :param freq: center of filter
+    :return: filtered data
+    """
+    #freqMin = int(freq * 0.9)
+    #freqMax = int(freq * 1.1)
+    #return bandstop(data, freqMin, freqMax, RATE, zerophase=True)
+    data = highpass(data, freq, RATE, zerophase=True)
+    print("indiceOfFundamental")
+    print(indiceOfFundamental)
+    return amplificationHarmonic(data)
+
+
+def callback(in_data, frame_count, time_info, flag):
+    """
+
+    :param in_data:
+    :param frame_count:
+    :param time_info:
+    :param flag:
+    :return: audio_data
+    """
+    global freq
+    audio_data = np.fromstring(in_data, dtype=np.float32)
+    # do processing here
+    # audioInFreq = fftBlack(audio_data)
+    # fulldata = abs(np.fft.ifft(harmonicMode(audioInFreq, findFreq(audioInFreq))))
+    # only for time domain
+    spec = abs(np.array(fftBlack(audio_data)))
+    freq = findFreq(spec)
+
+    if harmonic:
+
+        spec = harmonicMode(audio_data, freq)
+    else:
+        spec = audio_data
+    stream.write(spec)
+    return (audio_data, pa.paContinue)
+
+
+stream = soundObject.open(format=FORMAT,
+                          channels=CHANNELS,
+                          rate=RATE,
+                          input=True,
+                          output=True,
+                          frames_per_buffer=BUF_SIZE,
+                          stream_callback=callback)
+
+s=BluetoothSocket( RFCOMM )
+s.connect((HOST, PORT))
+
+if __name__ == '__main__':
+    while stream.is_active():
+        s.send()
+        print("actif")
+        #time.sleep(1)
+    stream.stop_stream()
+    stream.close()
+
+    soundObject.terminate()
